e11bbc5333472c3ccd9ddde9f555bfa18d3478c0
Moacir <moa.moda@gmail.com>
2012-01-12 11:30:39 -0200

    making two new models in medicine app

--- medlist/medicine/models.py
+++ medlist/medicine/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Medicine(models.Model):
 
+	def __unicode__(self):
+		return str(self.Abbreviation)
+
 	MedicineID = models.AutoField(primary_key=True)
 	Abbreviation = models.CharField('Abreviation', max_length=50, null=True)
 
@@ -23,6 +26,9 @@ class Medicine_Local(models.Model):
 	MedicineID = models.ForeignKey(Medicine,verbose_name='Medicine Identification')
 	MedicineName = models.CharField('Medicine Name', max_length=250)
 
+	def __unicode__(self):
+		return str(self.MedicineName)
+
 class MedicineRef(models.Model):
 
 	MedicineRefID = models.AutoField(primary_key=True)
@@ -31,21 +37,34 @@ class MedicineRef(models.Model):
 	MedicineRefDesc = models.TextField('Description')
 	MedicineRefType = models.CharField('Type', max_length=30, blank=True, null=True)
 	MedicineRefOrder = models.IntegerField('Order')
+
+	def __unicode__(self):
+		return str(self.MedicineID)
 	
 class MedicineAppStatus(models.Model):
 
 	MedicineAppStatusID = models.AutoField(primary_key=True)
 	MedicineAppStatusName = models.CharField('Name',max_length=10)
 
+	def __unicode__(self):
+		return str(self.MedicineAppStatusName)
+
 class MedicineAppType(models.Model):
 
 	MedicineAppTypeID = models.AutoField(primary_key=True)
+
+	def __unicode__(self):
+		return str(self.MedicineAppTypeID)
 
 class MedicineAppType_Local(models.Model):
 
 	MedicineAppTypeID = models.ForeignKey(MedicineAppType)
 	LangID = models.ForeignKey(Language,verbose_name='Language Identification')
 	MedicineAppTypeName = models.CharField('Name',max_length=50)
+
+	"""def __unicode__(self):
+		return str(self.MedicineAppTypeName)"""
+
 
 class MedicineApp(models.Model):
 
@@ -59,6 +78,29 @@ class MedicineApp(models.Model):
 	Sectionid = models.IntegerField('Section ID', blank=True, null=True)
 	ApplyToChildrenOnly = models.BooleanField('Apply to Children Only?', blank=True)
 	NotApplicableForChildren = models.BooleanField('Not Applicable For Children?', blank=True)
+
+class MedicineAppHistoryDate(models.Model):
+
+	MedicineAppHistoryDateID = models.AutoField(primary_key=True)
+	ChangeDate = models.CharField(max_length=100, blank=True, null=True)
+	EMLNumber = models.IntegerField(blank=True, null=True)
+	EMLYear = models.IntegerField(blank=True, null=True)
+	ExpComNumber = models.IntegerField(blank=True, null=True)
+
+class MedicineAppHistory(models.Model):
+
+	MedicineAppHistoryID = models.AutoField(primary_key=True)
+	MedicineAppRationale = models.TextField(null=True, blank=True)
+	ReasonforChange = models.TextField(null=True, blank=True)	
+	MedicineAppHistoryDateID = models.ForeignKey(MedicineAppHistoryDate)
+	MedicineAppStatusID = models.ForeignKey(MedicineAppStatus)
+	MedicineAppID = models.ForeignKey(MedicineApp)
+	MedicineID = models.ForeignKey(Medicine)
+	MedicineName = models.CharField(max_length=200, blank=True, null=True)		
+	Formulation = models.TextField(blank=True, null=True)
+	ATC_code = models.CharField(max_length=10, blank=True, null=True)
+	ModificationDate = models.DateTimeField(blank=True, null=True)
+	ModelListSection = models.CharField(blank=True, null=True, max_length=50)
 
 
 
