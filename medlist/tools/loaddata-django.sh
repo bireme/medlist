3dd7d15d0bf3b95866b55400ddb627dc9ab62223
Moacir <moa.moda@gmail.com>
2012-01-12 11:33:33 -0200

    organizing the loaddata script to import fixtures that already ok

--- medlist/tools/loaddata-django.sh
+++ medlist/tools/loaddata-django.sh
@@ -7,9 +7,12 @@ cd ..
 python manage.py loaddata fixtures/initial_data.json
 python manage.py loaddata tools/json-django/Language.json
 python manage.py loaddata tools/json-django/MedicineAppStatus.json
+python manage.py loaddata tools/json-django/Medicine.json
 python manage.py loaddata tools/json-django/MedicineApp.json
 python manage.py loaddata tools/json-django/MedicineAppType.json
-python manage.py loaddata tools/json-django/MedicineRef.json
-python manage.py loaddata tools/json-django/Medicine.json
+
+python manage.py loaddata tools/json-django/MedicineAppType_Local.json
+python manage.py loaddata tools/json-django/MedicineAppHistoryDate.json
+python manage.py loaddata tools/json-django/Medicine_Local.json
 
 cd ${CUR}
