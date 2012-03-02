6f96ca45e810079a3ee9e89e327c8b5bec4f2ba4
moacirmoda <moa.moda@gmail.com>
2012-03-02 15:46:19 -0300

    making a view that show a xml solr

--- medlist/api/views.py
+++ medlist/api/views.py
@@ -1,8 +1,24 @@
 from medlist.directory.models import *
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, HttpResponse
 
 def iahx(request):
 
-	medicines = get_object_or_404(Medicine.objects.all())
+	try:
+		medicines = Medicine.objects.all()
+	except:
+		return HttpResponse("Erro: Nenhum objeto foi encontrado")
 
-	return render_to_response('api/iahx.xml', medicines)
+	output = []
+	for med in medicines:
+		
+		forms = PharmaceuticalForm.objects.filter(medicine=med.id)
+		med_local = MedicineLocal.objects.filter(medicine=med.id)
+
+		tmp = {}
+		tmp['medicine'] = med
+		tmp['forms'] = forms
+		tmp['local'] = med_local
+
+		output.append(tmp)
+
+	return render_to_response('api/iahx.xml', {'medicines': output}, mimetype="Content-type: text/xml")
