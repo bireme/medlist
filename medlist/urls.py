3943399dee839569a2bb58dde8e4dfc2cac71dda
moacirmoda <moa.moda@gmail.com>
2012-03-06 18:20:17 -0300

    adding new project urls

--- medlist/urls.py
+++ medlist/urls.py
@@ -1,4 +1,8 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.defaults import patterns, include, url
+
+from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,5 +18,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^api/iahx/?$', 'medlist.api.views.iahx'),
+    url(r'^medicine/(\d+)/?$', 'medlist.directory.views.show_medicine'),
 )
+
+# static files in development
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
