44198ad26690761af1ba871f1e089febf2eacf8b
moacirmoda <moa.moda@gmail.com>
2012-03-08 18:59:40 -0300

    adding app list in list aplications

--- medlist/settings.py
+++ medlist/settings.py
@@ -145,6 +145,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'medlist.directory',
+    'medlist.list',
 )
 
 # A sample logging configuration. The only tangible logging
