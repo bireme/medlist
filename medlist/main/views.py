ebeab50ba00326537fe882649634aafd35bdf45a
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2012-03-12 13:44:43 -0300

    Translations files Use trans template tag on strings Use RequestContext on views that use internationalization (csrf_token)

--- medlist/main/views.py
+++ medlist/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from medlist.list.models import List
+from django.template import RequestContext
 
 def index(request):
 
@@ -7,9 +8,9 @@ def index(request):
 	country = lists.filter(is_country=True)
 	special = lists.filter(is_special=True)
 
-	dict_response = {
+	dict_response = RequestContext (request, {
 		'lists_country': country,
 		'lists_special': special,
-	}
+	})
 
 	return render_to_response("main/index.html", dict_response)
