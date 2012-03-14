1bcedca5f1b0f78c1dd92423a8c0eab0ae6784ae
moacirmoda <moa.moda@gmail.com>
2012-03-14 10:34:27 -0300

    fix admin url

--- medlist/urls.py
+++ medlist/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/?', include(admin.site.urls)),
     
     url(r'^api/iahx/?$', 'medlist.api.views.iahx'),
     url(r'^api/get_hierarchy_relation/(\d+)/?$', 'medlist.api.views.get_hierarchy_relation'),
