ca59b8605e6298089bac97f9e582e760c0ea011b
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2012-04-10 17:02:07 -0300

    Display flags information on the list (#49)

---
+++ medlist/static/js/footnotes.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    $('#obs_a').click(function() {
+        TINY.box.show({url: '/static/html/observation_a.html', animate:false,close:true, opacity: 20, topsplit: 3});
+    });
+
+    $('#obs_c1').click(function() {
+        TINY.box.show({url: '/static/html/observation_c1.html', animate:false,close:true, opacity: 20, topsplit: 3});
+    });
+
+    $('#obs_c2').click(function() {
+        TINY.box.show({url: '/static/html/observation_c2.html', animate:false,close:true, opacity: 20, topsplit: 3});
+    });
+
+    $('#obs_e').click(function() {
+        TINY.box.show({url: '/static/html/observation_e.html', animate:false,close:true, opacity: 20, topsplit: 3});
+    });
+
+});    
