7e6678fae1edd6154a75ead082591e2cb9b225f0
moacirmoda <moa.moda@gmail.com>
2012-05-14 14:27:24 -0300

    implementing compare lists with indexing, by whoosh

--- medlist/list/index.py
+++ medlist/list/index.py
@@ -5,13 +5,11 @@ from whoosh import store, fields, index
 from search import search
 from models import *
 
-# title=fields.TEXT(stored=True),
-#    content=fields.TEXT,
-#    url=fields.ID(stored=True, unique=True))
 WHOOSH_SCHEMA = fields.Schema(medicine=fields.TEXT(stored=True),
 								type=fields.TEXT(stored=True),
 								composition=fields.TEXT(stored=True),
 								list=fields.NUMERIC(stored=True),
+								medicine_id=fields.NUMERIC(stored=True),
 								id=fields.NUMERIC(stored=True, unique=True))
 
 def create_index(sender=None, **kwargs):
@@ -21,32 +19,64 @@ def create_index(sender=None, **kwargs):
 signals.post_syncdb.connect(create_index)
 
 def update_index(sender, instance, created, **kwargs):
+    # reading the index
     ix = index.open_dir(settings.WHOOSH_INDEX)
-    writer = ix.writer() 
+    # creating the writer object
+    writer = ix.writer()
 
+    # if already exists, append the list in field lists.
+    list_id = instance.section.list.id
+    docs = search(instance.pharmaceutical_form.id, field="id")
+    if docs and 'list' in docs[0]:
+        if not list_id in docs[0]['list']:
+            list = docs[0]['list'] 
+            list.append(list_id)
+        else:
+            list = docs[0]['list']
+    else:
+        list = [list_id]
 
-    if created:
+    # if don't exists, create the doc
+    if not docs:
         writer.add_document(medicine=instance.pharmaceutical_form.medicine.name,
-        					type=instance.pharmaceutical_form.pharmaceutical_form_type.name,
-        					composition=instance.pharmaceutical_form.composition,
-        					list=[instance.section.list.id],
-        					id=instance.pharmaceutical_form.id)
-        writer.commit()
+                            type=instance.pharmaceutical_form.pharmaceutical_form_type.name,
+                            composition=instance.pharmaceutical_form.composition,
+                            list=[instance.section.list.id],
+                            medicine_id=instance.pharmaceutical_form.medicine.id,
+                            id=instance.pharmaceutical_form.id)
+    # if exists, update doc
     else:   
-
-		list_id = instance.section.list.id
-		docs = search(instance.pharmaceutical_form.id, field="id")
-		if docs and 'list' in docs[0]:
-			if not list_id in docs[0]['list']:
-				list = docs[0]['list'] 
-				list.append(list_id)
-			else:
-				list = docs[0]['list']
-
-		writer.update_document(medicine=instance.pharmaceutical_form.medicine.name,
-							type=instance.pharmaceutical_form.pharmaceutical_form_type.name,
-							composition=instance.pharmaceutical_form.composition,
-							list=list,
-							id=instance.pharmaceutical_form.id)
-		writer.commit()
+        writer.update_document(medicine=instance.pharmaceutical_form.medicine.name,
+                            type=instance.pharmaceutical_form.pharmaceutical_form_type.name,
+                            composition=instance.pharmaceutical_form.composition,
+                            list=list,
+                            medicine_id=instance.pharmaceutical_form.medicine.id,
+                            id=instance.pharmaceutical_form.id)
+    writer.commit()
 signals.post_save.connect(update_index, sender=SectionPharmForm)
+
+def delete_index(sender, instance, using, **kwargs):
+    """
+    Removes a list from lists field on doc.
+    """
+    
+    ix = index.open_dir(settings.WHOOSH_INDEX)
+    writer = ix.writer()
+    
+    docs = search(instance.pharmaceutical_form.id, field="id")
+    if docs and 'list' in docs[0]:
+        list = docs[0]['list']
+        
+        try: list.remove(instance.section.list.id) 
+        except: pass
+
+        writer.update_document(medicine=instance.pharmaceutical_form.medicine.name,
+                            type=instance.pharmaceutical_form.pharmaceutical_form_type.name,
+                            composition=instance.pharmaceutical_form.composition,
+                            list=list,
+                            medicine_id=instance.pharmaceutical_form.medicine.id,
+                            id=instance.pharmaceutical_form.id)
+        writer.commit()
+
+
+signals.pre_delete.connect(delete_index, sender=SectionPharmForm)
--- medlist/list/views.py
+++ medlist/list/views.py
@@ -4,9 +4,11 @@ from medlist.list.models import *
 from django.shortcuts import HttpResponse, render_to_response, get_object_or_404
 from django.views.decorators.cache import cache_page
 from django.views.decorators.csrf import csrf_protect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template import RequestContext
 from django.http import Http404
 from django.core.cache import cache
+from search import search
 import settings
 
 def get_parents(id):
@@ -56,145 +58,46 @@ def compare(request):
 
 	output = {}
 
-	lists_special = List.objects.filter(published=True).exclude(type='c')
-	lists_country = List.objects.filter(published=True).filter(type='c')
+	# output lists to make form
+	output['lists_special'] = List.objects.filter(published=True).exclude(type='c')
+	output['lists_country'] = List.objects.filter(published=True).filter(type='c')
 
-	lists = []
+	list_of_lists = []
 	if 'lists' in request.GET and request.GET['lists'] != "":
-		lists = request.GET['lists'].split(',')
-		if 'null' in lists:
-			lists.remove("null")
-
-	cache_lists = ""
-	for list in lists:
-		cache_lists += "_%s" % list
-
-	# if not load some lists, the section_form becomes empty
-	section_forms = []
-	if len(lists) > 0:
-		section_forms = SectionPharmForm.objects.filter(section__list__id__in=lists).distinct()
-		section_forms = section_forms.order_by("pharmaceutical_form__medicine__name")
-
-	# removes all duplicated pharmaceutical form
-	forms = []
-	for sf in section_forms:
-		id = sf.pharmaceutical_form.id
-		if id in forms:
-			section_forms = section_forms.exclude(pk=sf.id)
-		else:
-			forms.append(id)
-
-	# check for cache using user selected lists on the name
-	selected_lists = cache.get('selected_lists%s' % cache_lists)
-	if not selected_lists:
-		# make structure of comparation
-		selected_lists = []
-		for list in lists:
-			try:
-				obj = List.objects.get(pk=list)
-				tmp = {'obj': obj, 'forms': []}
-
-				for sf in section_forms:
-					if len(SectionPharmForm.objects
-						.filter(section__list=obj)
-						.filter(pharmaceutical_form=sf.pharmaceutical_form)) > 0:
-
-						tmp['forms'].append(sf.pharmaceutical_form.id)
-
-				selected_lists.append(tmp)
-			except:
-				pass
-		cache.set('selected_lists%s' % cache_lists, selected_lists, settings.CACHE_TIMEOUT)
-	
-	matcheds = cache.get('matcheds%s' % cache_lists)
-	if not matcheds:
-		# list only matcheds in all lists selecteds
-		matcheds = {}
-		for sf in section_forms:
-			is_match = True
-			for list in selected_lists:
-				if not sf.pharmaceutical_form.id in list['forms']:
-					is_match = False
-
-			if is_match:
-				matcheds[sf.pharmaceutical_form.id] = True
-		
-		matcheds = matcheds.keys()
-		cache.set('matcheds%s' % cache_lists, matcheds, settings.CACHE_TIMEOUT)
-
-	# if only matched selected	
-	if 'only_matched' in request.GET and request.GET['only_matched'] == "true":		
-		
-		section_forms_only_matcheds = cache.get('section_forms_only_matcheds%s' % cache_lists)
-		if not section_forms_only_matcheds:
-			# filter the medicine that will be dislayed
-			for sf in section_forms:
-				if not sf.pharmaceutical_form.id in matcheds:
-					section_forms = section_forms.exclude(pk=sf.id)
-
-			cache.set('section_forms_only_matcheds%s' % cache_lists, section_forms, settings.CACHE_TIMEOUT)
-		else:
-			section_forms = section_forms_only_matcheds
-
-	# if only unmatched selected	
-	if 'only_unmatched' in request.GET and request.GET['only_unmatched'] == "true":		
-		
-		section_forms_only_unmatcheds = cache.get('section_forms_only_unmatcheds%s' % cache_lists)
-		if not section_forms_only_unmatcheds:
-			# filter the medicine that will be dislayed
-			for sf in section_forms:
-				if sf.pharmaceutical_form.id in matcheds:
-					section_forms = section_forms.exclude(pk=sf.id)		
-
-			cache.set('section_forms_only_unmatcheds%s' % cache_lists, section_forms, settings.CACHE_TIMEOUT)
-		else:
-			section_forms = section_forms_only_unmatcheds
-
-	# pagination
-	if 'items_per_page' in request.GET and request.GET['items_per_page'] > -1:
-		ITEMS_PER_PAGE = int(request.GET['items_per_page'])
+		# remove nulls from list
+		lists = request.GET['lists'].replace(',null', '').replace('null', '')
+		# make a list of the lists
+		list_of_lists = lists.split(',')
+		# Make a OR query to get all forms in these lists
+		lists = lists.replace(",", " OR ")
 	else:
-		ITEMS_PER_PAGE = settings.ITEMS_PER_PAGE
+		lists = ""
 
-	if ITEMS_PER_PAGE > 0:
-		
-		pagination = {}
-		start = 0
-		
-		total_pages = len(section_forms) / ITEMS_PER_PAGE
-		if (len(section_forms) % ITEMS_PER_PAGE) != 0:
-			total_pages += 1
+	# if MATCHEDS, make a AND query
+	if request.GET.get('only_matched') and request.GET.get('only_matched') == 'true':
+		lists = lists.replace("OR", "AND")
 
-		pagination['items'] = range(1, total_pages+1)
+	# if UNMATCHEDS, make a ANDNOT query
+	elif request.GET.get('only_unmatched') and request.GET.get('only_unmatched') == 'true':
+		lists = lists.replace("OR", "ANDNOT")
+	
+	# search these forms
+	pharmaceutical_forms = search(lists)
+	print len(pharmaceutical_forms)	
+	
+	# make pagination
+	paginator = Paginator(pharmaceutical_forms, settings.ITEMS_PER_PAGE)
+	if request.GET.get('page'):
+		page = request.GET.get('page')
+		try: pagination = paginator.page(page)
+		except EmptyPage: pagination = paginator.page(paginator.num_pages)
+	else:
+		pagination = paginator.page(1)
 		
-		if 'page' in request.GET and request.GET['page'] > 0:
-			try:
-				page = int(request.GET['page'])
-			except:
-				page = 1
-			
-			start = (ITEMS_PER_PAGE * page) - ITEMS_PER_PAGE
-		
-			if page > 1:
-				pagination['prev'] = page - 1
-
-			if page < total_pages:
-				pagination['next'] = page + 1
-
-			pagination['page'] = page
-		else:
-			pagination['page'] = 1			
-			pagination['next'] = 2
-		
-		finish = start + ITEMS_PER_PAGE
-		
-		output['pagination'] = pagination
-
-		section_forms = section_forms[start:finish]
-
-	output['lists_special'] = lists_special
-	output['lists_country'] = lists_country
-	output['section_forms'] = section_forms
-	output['selected_lists'] = selected_lists
+	# output all results
+	output['pharmaceutical_forms'] = pagination.object_list
+	output['lists'] = List.objects.filter(id__in=list_of_lists)
+	output['paginator'] = paginator
+	output['pagination'] = pagination
 
 	return render_to_response('list/compare.html', output, context_instance=RequestContext(request))
