7ee83a02e06bab911b5d0f12c72de799b1a1c58b
moacirmoda <moa.moda@gmail.com>
2012-11-27 12:23:20 -0200

    updating models from history

--- medlist/history/models.py
+++ medlist/history/models.py
@@ -5,29 +5,49 @@ from list.models import *
 
 class History(models.Model):
 
-	LIST_TYPES = (
-			('c', _('Country List')),
-			('p', _('PAHO List')),
-			('w', _('WHO List')),
-		)
-	LIST_SUBTYPES = (
-			('e', _('EML')),
-			('c', _('EML Children')),
-			('h', _('High Cost')),
-			('s', _('Strategic Fund')),
-		)
+    LIST_TYPES = (
+            ('c', _('Country List')),
+            ('p', _('PAHO List')),
+            ('w', _('WHO List')),
+        )
+    LIST_SUBTYPES = (
+            ('e', _('EML')),
+            ('c', _('EML Children')),
+            ('h', _('High Cost')),
+            ('s', _('Strategic Fund')),
+        )
 
-	class Meta:
-		verbose_name = _("history")
-		verbose_name_plural = _("histories")
+    class Meta:
+        verbose_name = _("history")
+        verbose_name_plural = _("histories")
 
-	name = models.CharField(_("list name"), max_length=255)
-	content = models.TextField(_("content serialized"))
-	year = models.CharField(_("list year"), max_length=4)
-	type = models.CharField(_("list type"), max_length=255, choices=LIST_TYPES)
-	subtype = models.CharField(_("list subtype"), max_length=255, choices=LIST_SUBTYPES, null=True, blank=True)
-	created = models.DateTimeField(_("date creation"), default=datetime.now, editable=False)
+    name = models.CharField(_("name"), max_length=255)
+    abbreviation = models.CharField(_("abbreviation"), max_length=50)
+    year = models.IntegerField(_("year of publication"))
+    edition = models.CharField(_("edition"), max_length=50, blank=True)
+    type = models.CharField(_("Type"), max_length=1, choices=LIST_TYPES)
+    subtype = models.CharField(_("sub-Type"), max_length=1, choices=LIST_SUBTYPES, null=True, blank=True)
+    published = models.BooleanField(_("published"), default=False)
+    obs = models.TextField(_("observation"), null=True, blank=True)
+    created = models.DateTimeField(_("date creation"), default=datetime.now, editable=False)
 
-	def __unicode__(self):
-		return unicode(self.name)
+    def __unicode__(self):
+        return unicode(self.name)
+
+class HistoryLocal(models.Model):
+
+    class Meta:
+        verbose_name = _("history translation")
+        verbose_name_plural = _("history translations")
+
+    list = models.ForeignKey(List, verbose_name=_("list"))
+    language = models.CharField(_("language"), max_length=10, choices=LANGUAGES_CHOICES)
+    name = models.CharField(_("name"), max_length=255)
+    obs = models.TextField(_("observation"), null=True, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.language)
+
+    
+
 
