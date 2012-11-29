b686d233ced6b461d61c9973f96079e9cd987dc1
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2012-11-29 10:15:54 -0200

    Make history fields read-only

--- medlist/history/admin.py
+++ medlist/history/admin.py
@@ -3,11 +3,12 @@ from models import *
 
 
 class HistoryLocalAdmin(admin.TabularInline):
+    readonly_fields = ('language', 'name', 'obs', 'xml', 'content',)
     model = HistoryLocal
     extra = 0
 
 class HistoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'year', 'created')
+    readonly_fields = ('name', 'abbreviation', 'obs', 'edition', 'type', 'subtype', 'year', 'created', 'xml', 'content')
     search_fields = ('name', 'subtype', 'type', 'year')
     list_filter = ('type', 'subtype', 'year')
     inlines = [HistoryLocalAdmin, ]
