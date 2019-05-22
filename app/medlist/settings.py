27d38c476dfbb2a69ee0e144b08ea44a804b73e0
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2019-05-22 17:15:36 -0300

    Ajustes para TIMEZONE e adiciona biblioteca requests no requirements

--- app/medlist/settings.py
+++ app/medlist/settings.py
@@ -103,11 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
